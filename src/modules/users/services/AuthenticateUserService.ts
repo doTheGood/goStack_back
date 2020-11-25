c623c0cdbc39ecfc46285f2969f32f5a599ed70c
Diogo Novaes <diogonovaes@gmail.com>
2020-11-25 09:35:41 +0100

    N4 Block 1 -> Architecture and testing in Node.js - finished the Group 1 about Architecture  and starting Group 2 about Testing.

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,27 +1,34 @@
 import { compare } from 'bcryptjs';
-import { getRepository } from 'typeorm';
+
 import { sign } from 'jsonwebtoken';
-import authConfig from '../config/auth';
-import AppError from '../errors/AppError';
+import authConfig from '@config/auth';
+import { inject, injectable } from 'tsyringe';
 
-import User from '../models/User';
+import AppError from '@shared/errors/AppError';
 
-interface Request {
+import User from '../infra/typeorm/entities/User';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
   email: string;
   password: string;
 }
 
-interface Response {
+interface IResponse {
   user: User;
   token: string;
 }
 
+@injectable()
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<Response> {
-    const usersRepository = getRepository(User);
-    const user = await usersRepository.findOne({
-      where: { email },
-    });
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError('Incorret email/password combination', 401);
